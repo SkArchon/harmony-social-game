2f349a295279856a8fe0e9b60454ac3d8bf56f90
Sk Arch <sk.arch7878@gmail.com>
2021-06-25 00:55:56 +0530

    Updated Contract

--- user-interface/src/app/app.constants.ts
+++ user-interface/src/app/app.constants.ts
@@ -1,4 +1,4 @@
-export const CONTRACT_ADDRESS = '0x84567Da205cDD993E74502bF9b387cD0d4531272';
+export const CONTRACT_ADDRESS = '0x409a0FC39F9292f7cd756F53181BFFc05927E575';
 export const HARMONY_URL = 'https://api.s0.b.hmny.io';
 
 
